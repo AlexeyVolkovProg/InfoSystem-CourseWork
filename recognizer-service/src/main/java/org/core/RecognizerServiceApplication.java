c058bac7a9d20656cd45644f6b750753286b3a4f
AlexVolkov <AlexVolkovProgrammer@yandex.com>
2024-11-26 13:03:58 +0300

    Initial Commit

---
+++ recognizer-service/src/main/java/org/core/RecognizerServiceApplication.java
@@ -0,0 +1,11 @@
+package org.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecognizerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RecognizerServiceApplication.class, args);
+    }
+}
